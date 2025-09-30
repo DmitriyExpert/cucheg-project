1305922745a81709b95c96502686d5f501811590
Demakov Dmitry <dpemvr@yandex.ru>
2025-09-30 15:56:16 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    minify: true,
+  },
+  publicDir: path.resolve(__dirname, './public'),
+});
